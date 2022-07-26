326b0c1551c6e00b7a617af6089bb104efc944a3
adarsh-addy <adarsh80singh@gmail.com>
2022-07-26 14:08:20 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Content1 from './components/Content1';
+
+function App() {
+  return (
+    <div className="App">
+      <Content1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Content1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import Maincontent from './Maincontent'
+import NavBar from './Navbar/Nav_bar'
+
+export default function Content1() {
+  return (
+    <div>
+        <NavBar/>
+        <Maincontent/>
+        <Footer/>
+    </div>
+  )
+}
